21c606fd21129c0e7d9ce34ec00190489d120170
nonarnee <nonarnee@gmail.com>
2021-04-14 21:33:09 +0900

    - html detail 태그로 context menu 구현

--- 1_Context-Menu/3_html-js/index.js
+++ 1_Context-Menu/3_html-js/index.js
@@ -1,4 +1,19 @@
 // Import stylesheets
 import "./style.css";
 
-// Write Javascript code here!
+const items = document.querySelectorAll('.details');
+
+document.body.addEventListener(function (e) {
+  if(e.target.nodeName !== 'P' && e.target.nodeName !== 'SUMMARY') {
+    items.forEach(function (item) {
+      item.removeAttribute('open');
+    })
+  }
+  items.forEach(function (item) {
+    if(item !== e.target.parentElement) {
+      item.removeAttribute('open');
+    }
+  })
+})
+
+// js가 동작하지 않는 경우에도 컨텐츠를 볼 수 있는 details, summary가 더 좋은 방법이 될 수 있다.
