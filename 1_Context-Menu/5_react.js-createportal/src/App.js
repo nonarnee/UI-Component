4a2143639949b94e2718101b0fe318c4f8de6bfa
nonarnee <nonarnee@gmail.com>
2021-04-14 21:58:49 +0900

    - react potal로 context menu 구현

--- 1_Context-Menu/5_react.js-createportal/src/App.js
+++ 1_Context-Menu/5_react.js-createportal/src/App.js
@@ -4,6 +4,11 @@ import Detail from "./Detail";
 import dummyData from "./dummyData";
 import ContextPortal from "./ContextPortal";
 
+// html, css로 해결할 수 있는 로직은 js로 처리하지 않는것이 좋다.
+// 1. js가 동작하지 않는 경우에도 완결성 있는 페이지를 보여줄 수 있다.
+// 2. 복잡한 컴포넌트를 개발할 때 개발자의 js 역량 보다는 html, css에서 검증된 기능을 활용하는 것이 안정성, 신뢰성이 높다.
+// 3. js 코드량이 줄어든다.
+
 export default function App() {
   const [openedIndex, setOpen] = useState(null);
   const detailRefs = useRef([]);
@@ -40,7 +45,10 @@ export default function App() {
         ))}
       </div>
 
-      <ContextPortal /* 채워 넣으세요. */ />
+      <ContextPortal
+        target={detailRefs.current[openedIndex]}
+        children={<p>{dummyData[openedIndex]?.context}</p>}
+      />
     </>
   );
 }
